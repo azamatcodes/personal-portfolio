cb44636af22b6bbfefdd2513e7eca17627611220
azamatcodes <azamatcodes@gmail.com>
2023-02-17 00:32:58 +0500

    Make main index.ts file for store.ts

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './styles/main.scss'
 import { Provider } from 'react-redux'
-import { store } from './store/store'
+import { store } from './store'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
